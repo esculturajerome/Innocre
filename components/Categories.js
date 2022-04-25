629404be545e004e67222f7117adaeda7c7034f0
Jerome Escultura <jescultura@deloitte.com>
2022-04-25 16:55:10 +0800

    Fixed ESlint issue

--- components/Categories.js
+++ components/Categories.js
@@ -4,7 +4,10 @@ function Categories() {
     <div className="overflow-y-hidden overflow-x-scroll scroll-hidden">
       <div className="inline-flex space-x-2">
         {categories.map((category) => (
-          <button className="border md:bg-[#f2f2f2] hover:border-black rounded-full px-4 md:py-1  focus:bg-[#222222] text-[#222222] focus:text-white">
+          <button
+            key={category}
+            className="border md:bg-[#f2f2f2] hover:border-black rounded-full px-4 md:py-1  focus:bg-[#222222] text-[#222222] focus:text-white"
+          >
             <span className="text-sm tracking-wider">{category}</span>
           </button>
         ))}
--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-import { BeakerIcon, MoonIcon, SunIcon } from "@heroicons/react/solid";
+// import { useState, useEffect } from "react";
+// import { useTheme } from "next-themes";
+// import { BeakerIcon, MoonIcon, SunIcon } from "@heroicons/react/solid";
 
 function header() {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  // const { systemTheme, theme, setTheme } = useTheme();
+  // const [mounted, setMounted] = useState(false);
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
 
-  const renderThemeChange = () => {
-    if (!mounted) return null;
-    const currentTheme = theme === "system" ? systemTheme : theme;
+  // const renderThemeChange = () => {
+  //   if (!mounted) return null;
+  //   const currentTheme = theme === "system" ? systemTheme : theme;
 
-    if (currentTheme === "dark") {
-      return (
-        <SunIcon
-          className="w-7 h-7"
-          role="button"
-          onClick={() => setTheme("light")}
-        />
-      );
-    } else {
-      return (
-        <MoonIcon
-          className="w-7 h-7"
-          role="button"
-          onClick={() => setTheme("dark")}
-        />
-      );
-    }
-  };
+  //   if (currentTheme === "dark") {
+  //     return (
+  //       <SunIcon
+  //         className="w-7 h-7"
+  //         role="button"
+  //         onClick={() => setTheme("light")}
+  //       />
+  //     );
+  //   } else {
+  //     return (
+  //       <MoonIcon
+  //         className="w-7 h-7"
+  //         role="button"
+  //         onClick={() => setTheme("dark")}
+  //       />
+  //     );
+  //   }
+  // };
   return (
     <div className="h-15 bg-white text-black py-4 flex justify-between items-center border-b md:w-[90%] m-auto">
       {/* {renderThemeChange()} */}
